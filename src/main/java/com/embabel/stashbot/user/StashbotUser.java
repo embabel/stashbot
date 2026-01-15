5d5d34cd151921411817e376b0375472cebe5526
Rod Johnson <johnsonroda@gmail.com>
2026-01-15 16:56:12 +1100

    Go back to Maven

--- src/main/java/com/embabel/stashbot/user/StashbotUser.java
+++ src/main/java/com/embabel/stashbot/user/StashbotUser.java
@@ -3,6 +3,9 @@ package com.embabel.stashbot.user;
 import com.embabel.agent.api.identity.User;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * User model for Stashbot.
  */
@@ -12,10 +15,31 @@ public class StashbotUser implements User {
     private final String displayName;
     private final String username;
 
+    private List<String> contexts = new LinkedList<>();
+
+    private String currentContext;
+
     public StashbotUser(String id, String displayName, String username) {
         this.id = id;
         this.displayName = displayName;
         this.username = username;
+        this.contexts.addAll(List.of("work", "personal"));
+        this.currentContext = "personal";
+    }
+
+    public List<String> getContexts() {
+        return contexts;
+    }
+
+    public String getCurrentContext() {
+        return currentContext;
+    }
+
+    public void setCurrentContext(String currentContext) {
+        if (!contexts.contains(currentContext)) {
+            contexts.add(currentContext);
+        }
+        this.currentContext = currentContext;
     }
 
     @Override
